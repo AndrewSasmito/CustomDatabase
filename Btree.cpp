cfcb8d77c924c3fce515632bd26c918aad5b8359
Andrew Sasmito <asasmito1920@gmail.com>
2025-05-02 15:00:12 -0400

    Initial implementation

---
+++ Btree.cpp
@@ -0,0 +1,34 @@
+#include "database.h"
+
+/*
+ * BTreeNode Constructor Implementation
+ */
+template <typename KeyType, typename ValueType>
+BTreeNode<KeyType, ValueType>::BTreeNode(bool leaf) : is_leaf(leaf) {
+    // Constructor implementation
+}
+
+/*
+ * BTree Constructor Implementation
+ */
+template <typename KeyType, typename ValueType>
+BTree<KeyType, ValueType>::BTree(int maxKeys) : maxKeysPerNode(maxKeys) {
+    root = new BTreeNode<KeyType, ValueType>(true);  // Initially, root is a leaf
+}
+
+/*
+ * Example Method to Insert Key-Value Pairs (Placeholder)
+ */
+template <typename KeyType, typename ValueType>
+void BTree<KeyType, ValueType>::insert(const KeyType& key, ValueType* value) {
+    // Method implementation for insert
+    // Insert logic for the BTree would go here
+}
+
+// Explicit template instantiation (to generate the specific versions of the template classes)
+template class BTreeNode<int, int>;
+template class BTree<int, int>;
+template class BTreeNode<std::string, int>;
+template class BTree<std::string, int>;
+template class BTreeNode<fraction, int>;
+template class BTree<fraction, int>;
