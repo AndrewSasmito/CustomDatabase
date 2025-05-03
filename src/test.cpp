72d7bbcdf8a594b49878691e1c47d1dcef9606cf
Andrew Sasmito <asasmito1920@gmail.com>
2025-05-03 00:00:17 -0400

    Made changes to code style

--- src/test.cpp
+++ src/test.cpp
@@ -2,6 +2,8 @@
 #include <cassert>
 #include "db/database.h"
 
+
+// If you return anything but 0, the github workflow will return an error!
 int main() { // Simple interface for now
     Database db; // Create db instance
 
@@ -9,11 +11,11 @@ int main() { // Simple interface for now
     db.createTable("users", {"id", "name"});
     Table* users = db.getTable("users");
 
-    assert(users);
-    users->insert({"1", "personA"});
-    users->insert({"2", "personB"});
+    assert(users); //Check that the users are saved
+    assert(users->insert({"1", "personA"}) == 0);
+    assert(users->insert({"2", "personB"}) == 0);
 
-    assert(users->printAll().compare("1 personA \n2 personB \n") == 0);
+    assert(users->printAll().compare("1 personA \n2 personB \n") == 0); //Check that the users are in the database
 
     std::cout << "All tests have successfully passed!\n";
     return 0;
