527893c1fa41b1b91e81d8a99f9cac4788f82c95
Andrew Sasmito <asasmito1920@gmail.com>
2025-05-03 00:00:17 -0400

    Made changes to code style

--- include/db/table.h
+++ include/db/table.h
@@ -7,7 +7,7 @@ class Table {
 public: // Constructor, to initialize the table with column names
     Table() = default;
     Table(const std::vector<std::string>& columns);
-    void insert(const std::vector<std::string>& values);
+    bool insert(const std::vector<std::string>& values);
     std::string printAll(); // Sample use of print function
 
 private: // Store column names and rows
