9e94413e06087058000e28dd05b0dc3a47f6e3ad
FionaV2400 <139073560+FionaVerzivolli@users.noreply.github.com>
2025-05-28 22:51:35 -0400

    deprecated table implementation

--- include/db/database.h
+++ include/db/database.h
@@ -1,15 +1,6 @@
 #pragma once
 #include <string>
 #include <unordered_map>
-#include "table.h"
 #include "btree.h"
 
-class Database {
-    public: // Create a table with the given name and columns
-        void createTable(const std::string& name, const std::vector<std::string>& columns);
-        Table* getTable(const std::string& name);
-
-
-    private: // Store tables in a map with the table name as the key
-        std::unordered_map<std::string, Table> tables;
-};
+class Database {};
