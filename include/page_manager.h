3f89270f72113d7f68bbf4e196fcc6e762785cee
FionaV2400 <139073560+FionaVerzivolli@users.noreply.github.com>
2025-05-28 23:16:24 -0400

    added tests

--- include/page_manager.h
+++ include/page_manager.h
@@ -13,6 +13,7 @@ struct PageHeader {
 };
 
 struct SlotEntry {
+    uint8_t id;
     uint16_t offset;  // Offset from start of page
     uint16_t length;  // Length of the record
     uint8_t is_deleted;  // Logical deletion flag
