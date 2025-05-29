adfce7259669d24ca6e91303dbfff3faebaab44b
FionaV2400 <139073560+FionaVerzivolli@users.noreply.github.com>
2025-05-28 22:51:35 -0400

    deprecated table implementation

--- include/page_manager.h
+++ include/page_manager.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <cstdint>
 #include <vector>
-
 #include "hash_util.h"
 
 struct PageHeader {
--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,5 @@
 
 int main() { // Simple interface for now
     Database db; // Create db instance
-
-    // Sample table layout
-    db.createTable("users", {"id", "name"});
-    Table* users = db.getTable("users");
-
-    if (users) { // Insert some basic stuff
-        users->insert({"1", "personA"});
-        users->insert({"2", "personB"});
-        users->printAll();
-    } else {
-        std::cerr << "Table not found\n"; // Error msg
-    }
-
     return 0;
 }
