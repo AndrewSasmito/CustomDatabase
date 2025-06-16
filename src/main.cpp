c28b9c47e4bd28f832e496f170b05b37e0803cac
Andrew Sasmito <asasmito1920@gmail.com>
2025-06-15 23:01:41 -0400

    Update the header

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "db/database.h"
 
 int main() { // Simple interface for now, incorrect for now
-    BTree<Page<int>, int> database(5);
+    //BTree<Page<int>, int> database(5);
 
     return 0;
 }
